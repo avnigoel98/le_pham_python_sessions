90a5b98032338ad1f5a213ed352dcccf0cd827ac
avnigoel98 <avni.goel@insight.com>
2024-04-30 20:12:00 +0530

    first commit

---
+++ input.py
@@ -0,0 +1,82 @@
+
+# name = input("Enter their name:")  # by defualt input is a string data type
+
+# print(name)
+
+# num1 = int(input("Enter your number:"))
+
+# print(type(num1))
+# print(num1 + 2)
+
+# we get -> "4" 
+# we want -> 4
+# so we type casted "4" (string datatype) to 4 (int datatype)
+
+
+# write a program to find remainder when a number is divided by 2
+
+# %  (modulus) -> it gives the remainder
+# /  (division) -> it gives you the qoitent
+
+# 9 / 2 -> 4
+# 9 % 2 -> 1 
+
+# a = 15
+# b = 2
+
+# print("The remainder when a is divided by b is: ", a % b)
+
+
+# Use comparision operators to find out whether a given variable a is greater than b or not
+# take a = 34 and b = 80
+
+
+# Write a program to find average of 2 numbers given by the user
+
+
+# strings
+
+
+# s1 = "hello" 
+
+# s2  = 'hello'
+
+# s3 = '''hello'''
+
+# print(s1, s2, s3)
+
+
+# print('Pham favourite language is "python".')
+
+
+# string slicing - taking some part of the string out
+
+# indexing - locations
+
+#index  01234567891011
+# name = "Good Morning"
+
+# print(name[0:11:2])
+
+# slicing (start : end - 1: skipping factor)
+
+
+# sentence = "harry is good in python"
+# print(sentence[::3])   # skips every 2 charcters
+
+# #       01234
+# name = "harry"
+# #       -5-4-3-2-1
+
+# # negative indexing
+# print(name[-4:-1])
+
+
+# write a program to display a user input name followed by Good afternonn using input () function
+
+# name = input("Enter your name: ")
+
+
+# print("Good Morning! " + name)
+
+
---
+++ session2.py
@@ -0,0 +1,66 @@
+# string functions
+
+# #       01234
+# name = "Aditi"
+
+# print(len(name))
+
+
+
+story = "once upon a time..."
+#        012345
+email = "avni@gmail.com"
+
+#print(email.endswith("com"))
+
+#print(email.count("a"))
+
+# print(story.capitalize())
+
+# print(email.find("hello")) # returns -1 if the string is not prsent
+# print(email.find("gmail")) # it returns 5 as g is coming at 5th location or index
+
+
+# caption  = '''developing writers can often benefit from examining an essay, a paragraph, or even a sentence to determine what makes it effective. On the following pages are several paragraphs for you to evaluate on your own, along with the Writing Center's explanation.
+
+# Note: These passages are excerpted from actual student papers and retain the original wording. Some of the sentences are imperfect, but we have chosen these passages to highlight areas in which the author was successful.
+
+# Thanks to the students who provided their writing for this page of our website. Do you have a discussion post, paper, or other writing assignment that you are particularly proud of? We would love to use it as a sample. Contact us at writingsupport@mail.waldenu.edu if you would like to share.'''
+
+
+
+# print(caption.count("the"))
+
+# print(caption.capitalize())
+
+# email = "avni@gmail.com"
+
+# print(email.replace("avni", "harry"))
+
+
+# \n - escape character for next line
+# \t - escape character for tab space
+
+story= "Once upon a time. \nThe python is a \tprogamming language"
+
+print(story)
+
+
+
+# Write a program to fill in a letter template given below with name and date
+
+letter = '''
+            Dear <|NAME|>
+                 You are selected!
+            <|DATE|> 
+        '''
+
+
+# Write a program to detect double spaces in a string
+"this is a   string"
+
+# replace that double space with single space
+
+# Write a program to format the following letter using escape characters
+
+letter = "Dear Harry, this python course is nice. Thanks"
