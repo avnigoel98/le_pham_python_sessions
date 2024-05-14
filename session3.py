af3b5a73daf2e7ed2f500b2b529341ba20be72f6
avnigoel98 <avni.goel@insight.com>
2024-05-14 19:48:35 +0530

    working on tuples

--- session3.py
+++ session3.py
@@ -13,10 +13,10 @@
 # list functions
 
 
-list1 = [90, 58, 77, 9, 34]
-print(list1)
-list1.sort()  # smallest to largest 
-print(list1)
+# list1 = [90, 58, 77, 9, 34]
+# print(list1)
+# list1.sort()  # smallest to largest 
+# print(list1)
 
 
 #      string    int  float  boolean
@@ -31,3 +31,49 @@ print(list1)
 # write a program to store 5 vegetables in a list entered by the user
 # write a program to accept marks of 3 students and display them in a sorted manner
 
+# list1 = "a, b, c, d, e"
+
+# print(type(list1))
+
+# list2 = ["a", "b", "c", "d", "e"]
+
+# print(type(list2))
+
+# vege1 = input("Enter vegetable 1: ")
+# vege2 = input("Enter vegetable 2: ")
+# vege3 = input("Enter vegetable 3: ")
+# vege4 = input("Enter vegetable 4: ")
+# vege5 = input("Enter vegetable 5: ")
+
+# list = [vege1, vege2, vege3, vege4, vege5]
+# print(list)
+
+# #         0   1  2   3  4
+# list1 = [90, 58, 77, 9, 34]
+# print(list1)
+# # list1.reverse() # reverses the list
+
+# # list1.append(100) # adding to the last of the list
+# # list1.insert(2, 500)  # adding at a particluar location or index 
+
+# #list1.pop(2) # deleting based on the index
+# # list1.remove(77) # deleting based on the value
+
+# print(list1)
+
+
+# write a program to sum a list with 4 numbers 
+
+# #        0    1   2  3
+# list1 = [2, 3, 1, 2]
+
+# sum = list1[0] + list1[1] + list1[2] + list1[3]
+
+# print(sum)
+
+
+# strings
+
+# list
+
+# tuples
---
+++ session4.py
@@ -0,0 +1,26 @@
+# both list and tuples can store multiple values
+# lists = []
+# you CAN update/change the list items 
+
+# tuples = ()
+# you CANNOT update/change the tuple items 
+
+# list1= [2, 4, 6, 7, 8]
+
+# print(list1)
+# list1[0] = 5
+# print(list1)
+
+# tuple1 = (4, 6, 3, 7, 8)
+
+# print(tuple1)
+# tuple1[0] = 5
+# print(tuple1)
+
+#         0   1   2  3  4
+tuple1 = (4, 600, 4, 4, 8)
+# tuple functions - count, index
+
+# print(tuple1.count(4)) # counts the number of times that value is present in the tuple
+
+# print(tuple1.index(600))  # give me the index of that value
