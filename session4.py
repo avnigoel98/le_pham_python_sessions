7c7bd103725c1794aad89b53d43a54270d443602
avnigoel98 <avni.goel@insight.com>
2024-05-14 20:00:11 +0530

    tuples continuation

--- session4.py
+++ session4.py
@@ -17,10 +17,27 @@
 # tuple1[0] = 5
 # print(tuple1)
 
-#         0   1   2  3  4
-tuple1 = (4, 600, 4, 4, 8)
-# tuple functions - count, index
+# #         0   1   2  3  4
+# tuple1 = (4, 600, 4, 4, 8)
+# # tuple functions - count, index
 
-# print(tuple1.count(4)) # counts the number of times that value is present in the tuple
+# # print(tuple1.count(4)) # counts the number of times that value is present in the tuple
 
-# print(tuple1.index(600))  # give me the index of that value
+# # print(tuple1.index(600))  # give me the index of that value
+
+
+# # write a program to count the number of zeros in a following tuple
+
+# a = (7, 0, 8, 0, 0, 9)
+
+
+tuple2 = () # empty tuple
+tuple1 = (2, ) # tuple with one value 
+
+
+print(tuple1)
+print(type(tuple1))
+
+
+# check that a tuple cannot be changed in python
+
